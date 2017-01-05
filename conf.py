5d76e62715f1c08e6deccffadf15afae6d465465
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-01-05 22:37:17 +0530

    Nikola auto commit.
    Nikola version: 7.7.11

--- conf.py
+++ conf.py
@@ -171,7 +171,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "material-theme"
+THEME = "lanyon"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
@@ -1204,7 +1204,7 @@ UNSLUGIFY_TITLES = True
 # If webassets is installed, bundle JS and CSS into single files to make
 # site loading faster in a HTTP/1.1 environment but is not recommended for
 # HTTP/2.0 when caching is used. Defaults to True.
-# USE_BUNDLES = True
+#USE_BUNDLES = True
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
